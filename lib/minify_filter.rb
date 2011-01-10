02019cbfeb83dbdf733fdba5cc348f221f73f7b0
Arjan van der Gaag <info@agwebdesign.nl>
2011-01-10 20:02:38 +0100

    Ruby 1.9 compatibility in minify filter

--- lib/minify_filter.rb
+++ lib/minify_filter.rb
@@ -7,8 +7,8 @@ class MinifyFilter < Nanoc3::Filter
   identifier :minify
   def run(content, args = {})
     case @item[:extension]
-    when 'css': Rainpress.compress(content)
-    when 'js': JSMin.minify(content)
+    when 'css' then Rainpress.compress(content)
+    when 'js' then JSMin.minify(content)
     else
       content
     end
