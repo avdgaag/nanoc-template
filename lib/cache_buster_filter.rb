2eb194f0e8c4f616476c1366f94831e2e3a069ae
Arjan van der Gaag <info@agwebdesign.nl>
2011-01-24 11:18:55 +0100

    Refactor cache busting.
    This introduces a different approach to cache
    busting. Rather than using server-side rewrites,
    now compile-time routing is used.
    
    • The cache buster filter now uses a hash of the
      file contents as fingerprint (was timestamp)
    • The Rules now output assets to a file with the
      fingerprint embedded in it.
    • The setting for turning cache-busting on and
      off is now gone.
    
    This makes the Rules file slightly more complex,
    but removes the need for slow server-side
    rewrites AND it removes a gap between a production
    and development environment.

--- lib/cache_buster_filter.rb
+++ lib/cache_buster_filter.rb
@@ -13,37 +13,57 @@
 class CacheBusterFilter < Nanoc3::Filter
   identifier :cache_buster
 
-  REGEX_CSS = /url\(('|"|)(([^'")]+)\.(gif|jpe?g|png|css|bmp))\1\)/i
-  REGEX_HTML = /(href|src)="([^"]+(\.(?:png|gif|js|jpe?g|css)))"/
+  # List of extensions of files we want to cache-bust
+  EXTENSIONS = %w{gif jpg jpeg png css bmp js}
+
+  # Regex for finding all references to files to be cache busted in CSS files
+  REGEX_CSS = /url\(('|"|)(([^'")]+)\.(#{EXTENSIONS.join('|')}))\1\)/i
+
+  # Regex for finding all references to files to be cache busted in HTML files
+  REGEX_HTML = /(href|src)="([^"]+(\.(?:#{EXTENSIONS.join('|')})))"/
 
   def run(content, args = {})
     stylesheet? ? bust_stylesheet(content) : bust_page(content)
+  end
+
+  # Get a unique fingerprint for a file's content. This currently uses
+  # an MD5 hash of the file contents.
+  #
+  # @param <String> path is the path to the file to fingerprint.
+  # @return <String> file fingerprint
+  def self.hash(path)
+    '-cb' + Digest::MD5.hexdigest(File.read(path))[0..8]
   end
 
 private
 
   # See if the current item is a stylesheet.
   # @todo This might need more or better criteria
+  # @return <Bool>
   def stylesheet?
     ['css', 'less'].include?(@item[:extension].to_s)
   end
 
-  # Get a nicely formatted timestamp for a file
-  def mtime(path)
-    File.mtime(path).strftime('%Y%m%d%H%m')
-  end
-
   # Make a path relative to the site's content dir absolute
+  # This works just like File.join, but makes the end result relative
+  # to the project directory (the directory one level above this file).
+  #
+  # @param <String> path part of a path
+  # @param ...
+  # @return <String> path
   def content_path(*path)
     File.join(File.dirname(__FILE__), '..', 'content', *path)
   end
 
   # Add cache-busters to stylesheets
+  #
+  # @param <String> content of a stylesheet to rewrite
+  # @return <String> rewritten content
   def bust_stylesheet(content)
     content.gsub(REGEX_CSS) do |m|
       real_path = content_path(File.dirname(@item.identifier), $2)
       if File.exists?(real_path)
-        m.sub($2, $3 + '-cb' + mtime(real_path) + '.' + $4)
+        m.sub($2, $3 + CacheBusterFilter.hash(real_path) + '.' + $4)
       else
         m
       end
@@ -53,6 +73,9 @@ private
   # Add cache-busters to HTML pages
   # @todo improve the check for less-turned-css files. This might be sass or
   #   scss. We could check this using the identifier...
+  #
+  # @param <String> content of a page to rewrite
+  # @return <String> rewritten content
   def bust_page(content)
     content.gsub(REGEX_HTML) do |m|
       attribute, path, extension = $1, $2, $3
@@ -62,7 +85,7 @@ private
       real_path.sub!(/css$/, 'less') unless File.exists?(real_path)
 
       if File.exists?(real_path)
-        %Q{#{attribute}="#{path.sub(extension, '-cb' + mtime(real_path) + extension)}"}
+        %Q{#{attribute}="#{path.sub(extension, CacheBusterFilter.hash(real_path) + extension)}"}
       else
         m
       end
